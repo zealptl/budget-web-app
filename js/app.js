357369ae3bd8e73bf4e475fea2c16a5ab3a25e55
zealptl <zealpatel06@gmail.com>
2020-01-20 21:36:30 -0500

    added methods to calculate budget and keep track of it

--- js/app.js
+++ js/app.js
@@ -12,6 +12,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var caclcTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(curr) {
+      sum += curr.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       inc: [],
@@ -20,7 +28,9 @@ var budgetController = (function() {
     totals: {
       inc: 0,
       exp: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -41,6 +51,29 @@ var budgetController = (function() {
 
       data.allItems[type].push(newItem);
       return newItem;
+    },
+
+    calcBudget: function() {
+      //1. Calculate total income and expenses
+      caclcTotal("inc");
+      caclcTotal("exp");
+      //2. Calculate budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+      //3. Calculate percentage of income spent
+      data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
+    },
+
+    displayData: function() {
+      console.log(data);
     }
   };
 })();
@@ -60,7 +93,7 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
@@ -120,20 +153,32 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
   };
 
+  var updateBudget = function() {
+    //1. Calculate the budget
+    budgetCtrl.calcBudget();
+    //2. Return the budget
+    var budget = budgetCtrl.getBudget();
+    //3. Display the budget on the UI
+    console.log(budget);
+  };
+
   var ctrlAddItem = function() {
     var input, newItem;
     //1. Get input data
     input = UICtrl.getInput();
-    //2. Add item to budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    //3. Add item to UI
-    UICtrl.addListItem(newItem, input.type);
 
-    //4. Clear fields
-    UICtrl.clearFields();
-    //5. Calculate the budget
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+      //2. Add item to budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      //3. Add item to UI
+      UICtrl.addListItem(newItem, input.type);
 
-    //6. Display the budget on the UI
+      //4. Clear fields
+      UICtrl.clearFields();
+
+      //5. Calculate and update budget
+      updateBudget();
+    }
   };
 
   return {
