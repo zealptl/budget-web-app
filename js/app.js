45775921267e428485544a9463aee1d56c5b40b7
zealptl <zealpatel06@gmail.com>
2020-01-21 21:28:24 -0500

    added method to delete item from UI

--- js/app.js
+++ js/app.js
@@ -138,6 +138,11 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
+    },
+
     clearFields: function() {
       var fields, fieldsArray;
       fields = document.querySelectorAll(
@@ -236,7 +241,7 @@ var controller = (function(budgetCtrl, UICtrl) {
       //1. Delete the item from the data structure
       budgetCtrl.deleteItem(type, ID);
       //2. Delete the item from UI
-
+      UICtrl.deleteListItem(itemID);
       //3. Update and show the new budget
     }
   };
